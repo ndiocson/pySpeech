5fc8126e87ed4efb3e448b344c1557ac2385cf2c
Nick Diocson <32041965+ndiocson@users.noreply.github.com>
2018-04-07 14:40:06 -0600

    Add files via upload

--- UI.py
+++ UI.py
@@ -1,25 +1,77 @@
 from tkinter import *
+from graph import Graph
+from word import Word
+from audio import *
+from word_methods import *
 
-root = Tk()
+master = Tk()
+
+master.geometry("700x500")
+master.title("pySpeech")
+
+L1 = Label(master, text="Enter Text Below: ")
+L1.grid(row=1, column=0)
+
+S = Scrollbar(master)
+S2 = Scrollbar(master)
+S.grid(row=2, column=1, sticky=NS)
+S2.grid(row=0, column=1, sticky=NS)
+
+entrytext = Text(master, width=95, height=10, bd=5)
+entrytext.grid(row=2, column=0)
+
+S.config(command=entrytext.yview)
+entrytext.config(yscrollcommand=S.set)
+
+entryfile = Entry(master, width=25, bd=5)
+entryfile.place(x=70, y=465)
+
+display = Text(master, width=95, height=19, bd=5)
+display.grid(row=0, column=0)
+display.configure(state=DISABLED)
+
+S2.config(command=display.yview)
+display.config(yscrollcommand=S2.set)
+
+def play(text):
+    '''
+    Takes a list of words and plays audio
+    '''
+    files = convert(text)
+    error = []
+    for f in files:
+        play_audio(str(f))
+
+def client_enter():
+    text = entrytext.get(1.0, END)[:-1]
+    display.configure(state=NORMAL)
+    display.delete(1.0, END)
+    display.insert('1.0', text)
+    display.configure(state=DISABLED)
+    play(text.split())
+
+def client_submit():
+    text = entryfile.get()
+    try:
+        file_object = open(text, 'r')
+        words = file_object.read()
+    except FileNotFoundError:
+        words = "File not found."
+
+    display.configure(state=NORMAL)
+    display.delete(1.0, END)
+    display.insert('1.0', words)
+    display.configure(state=DISABLED)
+    # L2 = Label(master, text=words, wraplength=50)
+    # for data in file_object:
+    #     words = data.split()
+    #     print(words)
 
 
-def printNote():
-    notify = Label(right, text="the button has been pressed")
-    notify.pack()
+enter = Button(master, text="Play", command=client_enter, bg="limegreen", activebackground="grey60")
+enter.place(x=5, y=465)
 
+submit = Button(master, text="Import", command=client_submit, bg="DodgerBlue2", activebackground="grey60")
+submit.place(x=285, y=465)
 
-left = Frame(root)
-left.pack(side=LEFT)
-
-right = Frame(root)
-right.pack(side=RIGHT)
-
-prompt = Label(left, text="Enter a sentence")
-play_button = Button(left, text="Play", command=printNote)
-text = Entry(left)
-
-prompt.grid(columnspan=2)
-text.grid(row=1)
-play_button.grid(row=1, column=1)
-
-root.mainloop()
+mainloop()
